dea5fdae129d83ac70f728ed2b0496aaaa342145
morfic <morfic@gmail.com>
2011-03-07 13:25:03 -0600

    gpfl

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 54 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#54 Sun Feb 27 16:55:16 CST 2011"
+#define LINUX_COMPILE_TIME "16:55:16"
+#define LINUX_COMPILE_BY "morfic"
+#define LINUX_COMPILE_HOST "othris"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
